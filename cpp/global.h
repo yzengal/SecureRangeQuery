0f39d23129bc954bf3c6500baf7424fa7dc66f0e
yzengal <yzengal@connect.ust.hk>
2024-05-16 15:19:28 +0800

    update all

---
+++ cpp/global.h
@@ -0,0 +1,130 @@
+#ifndef GRPC_COMMON_CPP_ICDE18_GLOBAL_H_
+#define GRPC_COMMON_CPP_ICDE18_GLOBAL_H_
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "ICDE18.grpc.pb.h"
+
+using ICDE18::Point;
+using ICDE18::Rectangle;
+using ICDE18::Circle;
+using ICDE18::CircleQueryRange;
+using ICDE18::RectangleQueryRange;
+using ICDE18::Record;
+using ICDE18::RecordSummary;
+using std::chrono::system_clock;
+
+namespace ICDE18 {
+
+enum QueryType_t {
+    UNDEFINED,
+    RANGE_QUERY,
+    RANGE_COUNT,
+    DISTANCE_JOIN,
+};
+
+struct Rectangle_t {
+    QueryType_t qtype;
+    float x, y;
+    float dx, dy;
+};
+
+struct Circle_t {
+    QueryType_t qtype;
+    float x, y;
+    float rad;
+};
+
+struct Record_t {
+    int ID;
+    float x, y;
+};
+
+// process spatial basic function
+//
+inline double GetDistance(const Point& a, const Point& b);
+inline double GetDistance(const Record_t& a, const Point& b);
+inline double GetDistance(const Record_t& a, const Record_t& b);
+inline double GetSquareDistance(const Point& a, const Point& b);
+inline double GetSquareDistance(const Record_t& a, const Point& b);
+inline double GetSquareDistance(const Record_t& a, const Record_t& b);
+inline bool IntersectWithRange(Const Point& a, const Rectangle& b);
+inline bool IntersectWithRange(Const Point& a, const Circle& b);
+inline bool IntersectWithRange(Const Record_t& a, const Rectangle& b);
+inline bool IntersectWithRange(Const Record_t& a, const Circle& b);
+inline bool IntersectWithRange(Const Record_t& a, const Rectangle_t& b);
+inline bool IntersectWithRange(Const Record_t& a, const Circle_t& b);
+
+// process read and write file
+//
+//
+std::string GetDataFilePath(int argc, char** argv);
+std::string GetQueryFilePath(int argc, char** argv);
+void GetInputData(const std::string& fileName, std::vector<Record_t>& recordVector);
+QueryType_t GetQueryType(const std::string& str);
+void GetInputQuery(const std::string& fileName, std::vector<Rectangle_t>& queries);
+void GetInputQuery(const std::string& fileName, std::vector<Circle_t>& queries);
+
+// process communication cost
+//
+//
+double CommRangeQuery(const Rectangle& a);
+double CommRangeQuery(const Circle& a);
+double CommQueryAnswer(const std::vector<Record>& a);
+double CommQueryAnswer(const RecordSummary& a);
+
+class QueryLogger {
+public:
+    QueryLogger() {
+        Init();
+    }
+
+    void Init() {
+        queryNum = 0;
+        queryTime = 0;
+        queryComm = 0;    
+        startTime = std::chrono::steady_clock::now();   
+        endTime = startTime; 
+    }
+
+    void SetStartTimer() {
+        startTime = std::chrono::steady_clock::now(); 
+    }
+
+    void SetEndTimer() {
+        endTime = std::chrono::steady_clock::now(); 
+    }
+
+    void LogOneQuery(double _queryComm=0.0f) {
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+        double _queryTime = duration.count();
+
+        queryNum += 1;
+        queryTime += _queryTime;
+        queryComm += _queryComm;
+
+        endTime = startTime;
+    }
+
+    void Print() {
+        double AvgQueryTime = (queryNum==0) ? 0 : (queryTime/queryNum);
+        double AvgQueryComm = (queryNum==0) ? 0 : (queryComm/queryNum);
+        printf("QueryNum = %d, AvgQueryTime = %.4lf, AvgQueryComm = %.4lf,", queryNum, AvgQueryTime, AvgQueryComm);
+        fflush(stdout);
+    }
+
+private:
+    std::chrono::steady_clock::time_point startTime, endTime;
+    int queryNum;
+    double queryTime;
+    double queryComm;
+};
+
+}  // namespace ICDE18
+
+#endif  // GRPC_COMMON_CPP_ICDE18_GLOBAL_H_
